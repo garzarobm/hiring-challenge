dca8223a6713dd45432b0a727a1b0b87e6917a27
Mike <garzarobm@gmail.com>
2025-08-31 03:32:16 -0500

    Update find_mars_code.awk and run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,15 @@
 set -eu
 
 # Run the AWK script to find the longest Mars mission security code
-LC_ALL=C ./find_mars_code.awk "${1:-space_missions.log}"
+# Usage: ./run.sh [verbose_level] [logfile]
+# verbose_level: 0=silent, 1=summary, 2=missions, 3=debug (default: 0)
+# logfile: path to space missions log (default: space_missions.log)
+
+VERBOSE=${1:-0}
+LOGFILE=${2:-space_missions.log}
+
+if [ "$VERBOSE" -eq 0 ]; then
+    LC_ALL=C ./find_mars_code.awk "$LOGFILE"
+else
+    LC_ALL=C ./find_mars_code.awk -v verbose="$VERBOSE" "$LOGFILE"
+fi
